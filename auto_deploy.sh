66a5ed75efdf811bff535c34c3a2d1ae71356d23
xrito <you@example.com>
2025-03-27 18:50:39 +0600

    Update deployment

---
+++ auto_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the current directory is a Git repository
+if [ ! -d ".git" ]; then
+    git init
+fi
+
+# Add all changes
+git add .
+
+# Commit with a provided message or default message
+commit_message=${1:-"Update deployment"}
+git commit -a -m "$commit_message"
+
+# Push to the main branch
+git push origin main
